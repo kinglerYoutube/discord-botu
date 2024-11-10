c22d36b6151fd9091feb5a61b6f50d836bebe374
kinglerYoutube <156936970+kinglerYoutube@users.noreply.github.com>
2024-11-10 22:13:36 +0000

    yenilemebot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,12 @@ import os
 from discord.ext import commands
 import discord
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-TKN = #"MThwMTYxOTU5MDA0ODI1MTkyNP.hsSrV_.LBUrZew3Veg48sQXE-lIXBIbAtrvObWipoı"
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+TKN = "MThwMTYxOTU5MDA0ODI1MTkyNP.hsSrV_.Zew3Veg48sQXE-lIXBIbAtrvObWuZWyBig"
+durum = None
 
 initial_extensions = []
 for filename in os.listdir("./cogs"):
@@ -21,10 +25,91 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.author.id == 948292425863036958:  # KULLANICI_ID ile kontrol etmek istediğiniz kullanıcının ID'sini yazın.
-        print(f"{message.author} kullanıcısının mesajı: {message.content}")
-    
-    await bot.process_commands(message)  # Botun komutlarını işlemesini sağlar    
+    if message.channel.id == 1304187472011460700 or message.channel.id == 1305252550488096829:
+        durum = None
+        log_channel_id_1 = 1305252550488096829
+        log_channel_1 = bot.get_channel(log_channel_id_1)
+
+        if message.author.id == 948292425863036958:  # KULLANICI_ID ile kontrol etmek istediğiniz kullanıcının ID'sini yazın.
+            print(f"{message.author} kullanıcısının mesajı: {message.content}")
+        
+        if message.author == bot.user:
+            return
+
+        if message.channel.id == log_channel_id_1:
+            if "merhaba" in message.content.lower() or "selam" in message.content.lower():
+                selection_1 = ["nasılsın", "yardımcı olmamı istermisin", "ne yapmamı istersin"]
+                selected_response = random.choice(selection_1)
+                await message.channel.send(selected_response)
+                if selected_response == "nasılsın":
+                    durum = "nasılsın_soruldu"
+                if selected_response == "yardımcı olmamı istermisin":
+                        durum = "yardım_teklifi_soruldu"
+
+            elif durum == "nasılsın_soruldu":
+                # Kullanıcı iyiyim veya kötüyüm dediğinde cevap ver
+                if "iyiyim" in message.content.lower():
+                    await message.channel.send("Mükemmel, sana nasıl yardım edebilirim?")
+                    durum = None  # Durumu sıfırla
+                elif "kötüyüm" in message.content.lower():
+                    await message.channel.send("Olsun, sana yardım edebilirim.")
+                    durum = None  # Durumu sıfırla
+            
+            elif durum == "yardım_teklifi_soruldu":
+                # Kullanıcı iyiyim veya kötüyüm dediğinde cevap ver
+                if "evet" in message.content.lower():
+                    await message.channel.send("Mükemmel,sadece 'yardım et' yaz")
+                    durum = None  # Durumu sıfırla
+                elif "hayır" in message.content.lower():
+                    await message.channel.send("tamam.")
+                    durum = None  # Durumu sıfırla
+
+            elif "yardım et" in message.content.lower():
+                await message.channel.send("Tabiki komutlar şunlardır")
+                time.sleep(0.5)
+                await message.channel.send("İlk komut (!selamver),komut selam veriyor yani (!selamver),yazarsan 'merhaba dünya'der.")
+                time.sleep(0.5)
+                await message.channel.send("İkinci komut (!selamla + isim),komut isme selam veriyor yani (!selamla kinglerYT),yazarsan 'selam kinglerYT'der.")
+                time.sleep(0.5)
+                await message.channel.send("Üçüncü komut (!say + çümle),komut botun senin yazdığın çümleyi yazmasını sağlar yani(!say kinglerYT),yazarsan 'kinglerYT'der.")
+                time.sleep(0.5)
+                await message.channel.send("Dördünçü komut (!saat_kac),komut adı üstünde saatin kaç olduğunu yazıyor yani (!saat_kac),yazarsan 'saat:dakika'der.")
+                time.sleep(0.5)
+                await message.channel.send("Beşinci komut (!google_ara + aramak istediğin şey),komut google'yi açıp aramak istediğin şeyi arar yani (!google_ara kinglerYT),yazarsan 'kinglerYT'yi google'de arar.")
+                time.sleep(0.5)
+                await message.channel.send("Altıncı komut (!youtube_ara + aramak istediğin şey),komut youtube'yi açıp aramak istediğin şeyi arar yani (!youtube_ara kinglerYT),yazarsan 'kinglerYT'yi youtube'de arar.")
+
+            elif "teşekkür ederim" in message.content.lower() or "teşekkürler" in message.content.lower():
+                selection_3 = ["rica ederim", "ne demek", "her zaman buradayım"]
+                selected_response = random.choice(selection_3)
+                await message.channel.send(selected_response)    
+
+            elif "hangi gündeyiz" in message.content.lower() or "bugün günlerden ne" in message.content.lower():
+                selection_5 = ["hemen bakıyorum:", "bugün günlerden:", "bugün:"]
+                selected_response = random.choice(selection_5)
+                today = time.strftime("%A").capitalize()
+                days = {
+                    "Monday": "pazartesi", "Tuesday": "salı", "Wednesday": "çarşamba",
+                    "Thursday": "perşembe", "Friday": "cuma", "Saturday": "cumartesi", "Sunday": "pazar"
+                }
+                today = days.get(today, today)
+                await message.channel.send(selected_response + today)
+
+            elif "saat kaç" in message.content.lower():
+                selection = ["saat şu an:", "hemen bakıyorum:"]
+                selected_response = random.choice(selection)
+                clock = datetime.now().strftime("%H:%M")
+                await message.channel.send(selected_response + clock)
+
+            elif "özel birşey aç" in message.content.lower():
+                await message.channel.send("tamam'dır bende o iş ")
+                time.sleep(1)
+                webbrowser.get().open("https://open.spotify.com/intl-tr/album/3QvP20url5kdwLj79EmWY1?uid=a7c830e545a8a0384cc3&uri=spotify%3Atrack%3A1sBDNj0H3XaL9uXnyeyUY7")
+                webbrowser.get().open("https://www.arthipo.com/image/cache/catalog/poster/mustafa-kemal-ataturk/mka153-mustafa-kemal-ataturk-bayrakla-gokyuzune-bakarken-bayrakli-portre-42-844x1200.webp")
+                time.sleep(1)
+                await message.channel.send("geçmiş on kasım kutlu olsun")
+
+    await bot.process_commands(message)
 
 @bot.event
 async def on_member_join(member: discord.member):
@@ -58,3 +143,354 @@ async def on_guild_channel_delete(channel: discord.TextChannel):
     
 
 bot.run(TKN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import time
+from datetime import datetime
+import random
+from random import choice
+import webbrowser
